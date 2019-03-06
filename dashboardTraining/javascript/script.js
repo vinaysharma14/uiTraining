dc2375d2a5ee19e3b96393aa2a77d11f5885fcde
vinaysharma14 <vinay@geekyants.com>
2019-03-06 10:36:38 +0530

    jQuery Initiated

---
+++ dashboardTraining/javascript/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function()
+{
+    $("#hamburger").click(function()
+    {
+        if($("#dashHead").css("left")!=0)
+        {
+            /* alert("1"); */
+            $("#sideNavBar").toggle();
+            $("#dashHead").css("left","0");
+            $("#dashHead").css("transition","0.5s");
+            $(".userInteraction").css("margin-left","-220px");
+            $(".userInteraction").css("transition","0.5s");
+        }
+        else if($("#dashHead").css("left")==0)
+        {
+            /* alert("2"); */
+            $("#sideNavBar").toggle();
+            $("#dashHead").css("left","220px");
+            $("#dashHead").css("transition","0.5s");
+            $(".userInteraction").css("margin-left","220px");
+            $(".userInteraction").css("transition","0.5s"); 
+        }
+    });
+});
